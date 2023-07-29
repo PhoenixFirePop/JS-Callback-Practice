68cae138d35a0e848ce361d88b4be537f14aa654
Diana T <135925668+PhoenixFirePop@users.noreply.github.com>
2023-07-29 09:42:36 -0500

    movement

--- move.js
+++ move.js
@@ -6,6 +6,32 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
+    function moveWithArrowKeys(left, bottom){
+    let direction = null;
+    let x = left;
+    let y = bottom;
+
+    element.style.left = x + 'px'
+    element.style.bottom = y + 'px'
+    
+    function moveCharacter(){ 
+        if(direction === 'west'){
+            x-=1
+        }
+        if(direction === 'north'){
+            y+=1
+        }
+        if(direction === 'east'){
+            x+=1
+        }
+        if(direction === 'south'){
+            y-=1
+        }
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+    }
+
+
     return {
         to: moveToCoordinates
     }
